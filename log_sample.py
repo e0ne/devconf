67f90929237bd78376d4a81a8fa3eb65f3d194c5
e0ne <e0ne@e0ne.info>
2013-06-14 00:12:08 +0400

    Set log format

--- log_sample.py
+++ log_sample.py
@@ -4,11 +4,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 streamHandler = logging.StreamHandler()
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+
 logger.addHandler(streamHandler)
 
 
 def main():
-    logger.info('main: started')
+    logger.info('started')
 
 if __name__ == '__main__':
     main()
