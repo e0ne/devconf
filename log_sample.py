893944795aa5a2a1ff0e1b231a174d38223bdd95
e0ne <e0ne@e0ne.info>
2013-06-14 00:46:26 +0400

    Log method call

--- log_sample.py
+++ log_sample.py
@@ -22,10 +22,39 @@ def log_call(func):
     return wrapper
 
 
+def log_method_call(method):
+    def wrapper(self, *args, **kwargs):
+        logger.debug('Called method: {0}'.format(method))
+        retval = getattr(self, '_logged_%s' % method)(*args, **kwargs)
+        return retval
+    return wrapper
+
+
+class MetaLogger(type):
+    def __new__(cls, classname, bases, classdict):
+        for attr, item in classdict.items():
+            if callable(item):
+                classdict['_logged_%s' % attr] = item
+                classdict[attr] = log_method_call(attr)
+
+        return super(MetaLogger, cls).__new__(cls, classname, bases, classdict)
+
+
+class VeryImportantClass(object):
+    __metaclass__ = MetaLogger
+
+    def output(self, message):
+        print('Very important output: {0}'.format(message))
+
+
 @log_call
 def main(*args):
     logger.info('started')
     print('Hello DevConf')
+
+    vic = VeryImportantClass()
+    vic.output(':)')
+
 
 if __name__ == '__main__':
     main(sys.argv)
