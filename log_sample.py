ff1aa9ad4abc05cbacb3af26a1252a62b69c596b
e0ne <e0ne@e0ne.info>
2013-06-14 00:26:30 +0400

    Log function call

--- log_sample.py
+++ log_sample.py
@@ -1,18 +1,31 @@
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 streamHandler = logging.StreamHandler()
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(LOG_FORMAT)
 streamHandler.setFormatter(formatter)
 
 logger.addHandler(streamHandler)
 
 
-def main():
+def log_call(func):
+    def wrapper(*args, **kwargs):
+        log_entry = 'Function "{0}" called with params args={1}, kwargs={2}'\
+            .format(func.func_name, args, kwargs)
+        logger.debug(log_entry)
+        func(*args, **kwargs)
+    return wrapper
+
+
+@log_call
+def main(*args):
     logger.info('started')
+    print('Hello DevConf')
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
